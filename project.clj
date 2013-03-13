53f61cf1f4a8643469ae5028403e1b732208076c
Myles Megyesi <mylesmegyesi@me.com>
2013-03-13 08:47:02 -0500

    updates the project description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject sass "3.2.6"
-  :description "The Sass compiler"
+  :description "Clojure bindings for the Ruby Sass compiler"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.0"]
