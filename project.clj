feb61786a35ad4f3bb74c578cc8a77b69a03682c
Myles Megyesi <mylesmegyesi@users.noreply.github.com>
2016-12-02 13:39:49 -0600

    Refactors to use jsass instead of Ruby Sass gem

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.0"]
                  [zweikopf "0.1.0"]
-                 [chee "1.1.2"]]
+                 [chee "1.1.2"]
+                 [io.bit3/jsass "5.3.0"]]
   :profiles {:dev {:dependencies [[speclj "2.5.0"]]
                    :resource-paths ["test_fixtures"]
                    :main speclj.main}}
