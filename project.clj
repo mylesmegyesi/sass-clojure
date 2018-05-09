289be01fe8a382297d713544673ba8f47ab66b59
Jay Zawrotny <jayzawrotny@gmail.com>
2018-05-09 11:18:13 -0400

    Updated libsass to newest version and ran tests

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [io.bit3/jsass "5.3.0"]]
+                 [io.bit3/jsass "5.5.3"]]
   :profiles {:dev {:dependencies [[speclj "3.3.2"]]
                    :resource-paths ["test_fixtures"]
                    :main speclj.main}}
